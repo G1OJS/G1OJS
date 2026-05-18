5197d76f9d1b59627a6dae6146b9dfef17863014
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-05-18 09:11:51 +0100

    update nec files

--- shorted_stub.py
+++ shorted_stub.py
@@ -1,15 +1,4 @@
 import numpy as np
 
-f = 7.07
-l = 0.66*40/4
-vf = 0.66
-wl = 300/f
-bl = 2*np.pi*l/(wl*vf)
-print(bl)
-
-x = 50*np.tan(bl)
-
-w = 2*np.pi*f*1e6
-ind = x/w
-
-print(x, ind/1e-6)
+for i in range(100):
+    print(i % 8)
